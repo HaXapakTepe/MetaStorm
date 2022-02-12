850ce07578648b4a589823dad03206984bc1a8e2
HaXapakTepe <haxapaktepe@gmail.com>
2022-02-12 18:41:07 +0400

    added slider-bottom, features, concept

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,73 @@
-$ (function() {
+$(function () {
 
-$('.top-slider__inner').slick({
-  dots: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+  $('.top-slider__list').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    adaptiveHeight: true,
+    useTransform: true,
+    speed: 900,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
+  $('.slider-bottom__list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    fade: false,
+    adaptiveHeight: true,
+    infinite: false,
+    useTransform: true,
+    speed: 400,
+    cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+  });
+
+  $('.slider-bottom__content')
+    .on('init', function (event, slick) {
+      $('.slider-bottom__content .slick-slide.slick-current').addClass('is-active');
+    })
+    .slick({
+      slidesToShow: 9,
+      arrows: false,
+      dots: false,
+      focusOnSelect: false,
+      infinite: false,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      }, {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      }, {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      }]
+    });
+
+  $('.slider-bottom__list').on('afterChange', function (event, slick, currentSlide) {
+    $('.slider-nav').slick('slickGoTo', currentSlide);
+    var currrentNavSlideElem = '.slider-bottom__content .slick-slide[data-slick-index="' + currentSlide + '"]';
+    $('.slider-bottom__content .slick-slide.is-active').removeClass('is-active');
+    $(currrentNavSlideElem).addClass('is-active');
+  });
+
+  $('.slider-bottom__content').on('click', '.slick-slide', function (event) {
+    event.preventDefault();
+    var goToSingleSlide = $(this).data('slick-index');
+
+    $('.slider-bottom__list').slick('slickGoTo', goToSingleSlide);
+  });
 
 });
